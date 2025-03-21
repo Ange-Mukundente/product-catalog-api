d640299ddf4ba8eadfac05ca2bcbb80906d2c7ca
Ange Mukundente <127440144+Ange-Mukundente@users.noreply.github.com>
2025-03-21 10:07:20 +0200

    product-catalog-api

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 const connectDB = require('./src/config/db');
 const productRoutes = require('./src/routes/productRoutes');
+const categoryRoutes = require('./src/routes/categoryRoutes');
 
 const app = express();
 connectDB();
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(morgan('dev'));
 
 app.use('/api/products', productRoutes);
+app.use('/api/categories', categoryRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
